4a11f959ffb30ef66a781d6e1debe114eac72ced
oladele90 <oladelemiro07@gmail.com>
2022-12-07 08:49:45 -0800

    modified files to handle non interactive mode

--- main.c
+++ main.c
@@ -5,22 +5,14 @@
  *Return: 0 on success
  */
 
-int main(int ac, char **av)
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
 	char *l_s;
 	char **split_toks;
 
-	if (ac > 1)
-		_execute(av);
 	while (1)
 	{
-		printf("($) ");
 		l_s = _getline();
-		if (l_s == NULL)
-		{
-			perror("ERROR");
-			exit(0);
-		}
 		split_toks = _split_toks(l_s);
 		if (split_toks[0] != NULL)
 			_execute(split_toks);
