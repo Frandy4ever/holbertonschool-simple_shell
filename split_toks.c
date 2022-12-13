65e961ba30bb7347e56ba870048ef1f310b38071
oladele90 <oladelemiro07@gmail.com>
2022-12-12 16:54:12 -0800

    split toks

--- split_toks.c
+++ split_toks.c
@@ -9,17 +9,17 @@ char **_split_toks(char *line)
 {
     int length = 0;
     int capacity = 16;
-    char **tokens = malloc(capacity * sizeof(char*));
+    char **tokens = malloc(capacity * sizeof(char *));
 
     char *delim = " \n";
     char *token = strtok(line, delim);
 
     while (token != NULL)
     {
-        tokens[length] = token;
-        length++;
+	    tokens[length] = token;
+	    length++;
 
-        token = strtok(NULL, delim);
+	    token = strtok(NULL, delim);
     }
 
     tokens[length] = NULL;
